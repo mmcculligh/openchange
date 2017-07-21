b0541c55fb3fc9daf005d81c79e51af390970766
Murray McCulligh <murray.mcculligh@cord3inc.com>
2017-07-21 15:26:10 -0400

    Disable code that performs a NDR push since it never checks for errors and it doens't seem to do anything.

--- mapiproxy/dcesrv_mapiproxy.c
+++ mapiproxy/dcesrv_mapiproxy.c
@@ -63,6 +63,8 @@ static NTSTATUS mapiproxy_op_connect(struct dcesrv_call_state *dce_call,
 	bool 					anonymous;
 	bool					no_kerberos;
 
+	const char				*oa_binding = NULL;
+
 	OC_DEBUG(5, "mapiproxy::mapiproxy_op_connect");
 
 	/* Retrieve the binding string from parametric options if undefined */
@@ -85,6 +87,14 @@ static NTSTATUS mapiproxy_op_connect(struct dcesrv_call_state *dce_call,
 
 	/* Retrieve private mapiproxy data */
 	private = dce_call->context->private_data;
+
+	if (private->oa_mode) {
+		oa_binding = lpcfg_parm_string(dce_call->conn->dce_ctx->lp_ctx, NULL, "dcerpc_mapiproxy", "oa_binding");
+		if (!binding) {
+			OC_DEBUG(0, "You must specify a DCE/RPC OutlookAnywhere binding string");
+			return NT_STATUS_INVALID_PARAMETER;
+		}
+	}
 
 	if (anonymous) {
 		OC_DEBUG(5, "dcerpc_mapiproxy: RPC proxy: Using anonymous credentials");
@@ -245,13 +255,27 @@ static NTSTATUS mapiproxy_op_connect(struct dcesrv_call_state *dce_call,
 	/* Always use the local binding parsing code path since we need to control the flags and assoc_group_id */
 	if (true) {
 		struct dcerpc_binding		*b;
+		struct dcerpc_binding 		*temp;
 		struct composite_context	*pipe_conn_req;
 
 		/* parse binding string to the structure */
-		status = dcerpc_parse_binding(dce_call->context, binding, &b);
-		if (!NT_STATUS_IS_OK(status)) {
-			oc_log(OC_LOG_ERROR, "dcerpc_mapiproxy: Failed to parse dcerpc binding '%s'", binding);
-			return status;
+		if (oa_binding) {
+			status = dcerpc_parse_binding(dce_call->context, oa_binding, &b);
+			if (!NT_STATUS_IS_OK(status)) {
+				oc_log(OC_LOG_ERROR, "dcerpc_mapiproxy: Failed to parse dcerpc binding '%s'", oa_binding);
+				return status;
+			}
+
+			status = dcerpc_parse_binding(dce_call->context, binding, &temp);
+
+			b->target_hostname = temp->target_hostname;
+		}
+		else {
+			status = dcerpc_parse_binding(dce_call->context, binding, &b);
+			if (!NT_STATUS_IS_OK(status)) {
+				oc_log(OC_LOG_ERROR, "dcerpc_mapiproxy: Failed to parse dcerpc binding '%s'", binding);
+				return status;
+			}
 		}
 
 		/* Enable multiplex on the RPC pipe */
@@ -596,7 +620,7 @@ static NTSTATUS mapiproxy_op_ndr_push(struct dcesrv_call_state *dce_call, TALLOC
 				mapiproxy_RfrGetNewDSA(dce_call, (struct RfrGetNewDSA *)r);
 				break;
 			default:
-				OC_DEBUG(0, "exchange_ds_rfr: OTHER DS-RFR CALL DETECTED!");
+				//OC_DEBUG(0, "exchange_ds_rfr: OTHER DS-RFR CALL DETECTED!");
 				break;
 			}
 		}
@@ -767,7 +791,7 @@ static NTSTATUS mapiproxy_op_dispatch(struct dcesrv_call_state *dce_call, TALLOC
 
 	if (private->server_mode == false) {
 	ahead:
-		if (mapiproxy.ahead == true) {
+/*		if (mapiproxy.ahead == true) {
 			push = ndr_push_init_ctx(dce_call);
 			NT_STATUS_HAVE_NO_MEMORY(push);
 			ndr_err = call->ndr_push(push, NDR_OUT, r);
@@ -776,7 +800,7 @@ static NTSTATUS mapiproxy_op_dispatch(struct dcesrv_call_state *dce_call, TALLOC
 				dce_call->fault_code = DCERPC_FAULT_NDR;
 				return NT_STATUS_NET_WRITE_FAULT;
 			}
-		}
+		}*/
 		
 		status = mapiproxy_module_dispatch(dce_call, mem_ctx, r, &mapiproxy);
 		if (!NT_STATUS_IS_OK(status)) {
